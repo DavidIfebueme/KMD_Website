ea9811c4335550352f16ec9ac31aad9b2ec8ebd8
DavidIfebueme <davidifebueme@gmail.com>
2023-07-06 00:20:44 +0100

    Made Books Model with model fields

--- kmd_website/books/models.py
+++ kmd_website/books/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField(blank=True)
+    amazon_link = models.URLField()
